d5b197b7b2e965e8e7609486ef155aead38eef68
metasharp <1176758572@qq.com>
2026-03-12 14:21:01 +0800

    为切面上下文增加函数参数

--- aspect-examples/src/async_func.rs
+++ aspect-examples/src/async_func.rs
@@ -1,5 +1,7 @@
-use aspect_core::{Aspect, AsyncAspect, AsyncJoinPoint, JoinPoint};
-use aspect_macros::{aspect, async_aspect};
+use std::any::Any;
+use std::future::Future;
+use aspect_core::{Aspect, AspectError, AsyncAspect, AsyncJoinPoint, AsyncProceedingJoinPoint, JoinPoint, ProceedingJoinPoint};
+use aspect_macros::aspect;
 use aspect_std::{LoggingAspect, TimingAspect};
 
 #[tokio::main]
@@ -12,13 +14,13 @@ async fn main() {
     println!("Result of sub: {}\n", result);
 }
 
-#[async_aspect(Logger)]
+#[aspect(Logger)]
 async fn add(a: i32, b: i32) -> i32 {
     println!("  [APP] Adding {} + {}", a, b);
     a + b
 }
 
-#[async_aspect(Logger)]
+#[aspect(Logger1)]
 async fn sub(a: i32, b: i32) -> i32 {
     println!("  [APP] Subtracting {} - {}", a, b);
     a - b
@@ -27,7 +29,57 @@ async fn sub(a: i32, b: i32) -> i32 {
 #[derive(Default)]
 pub struct Logger;
 
-impl AsyncAspect for Logger {
+impl Aspect for Logger {
+    fn before(&self, ctx: &JoinPoint) {
+        let args = ctx
+            .args
+            .iter()
+            .map(|arg| {
+                if let Some(v) = arg.downcast_ref::<i32>() {
+                    format!("{:?}", v)
+                } else {
+                    format!("{:?}", "<non-debug-arg>")
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        println!(
+            "before {}: {},{},{},[{}]",
+            ctx.function_name, ctx.module_path, ctx.location.file, ctx.location.line, args
+        );
+    }
+
+    fn after(&self, _ctx: &JoinPoint, _result: &dyn Any) {
+        let args = _ctx
+            .args
+            .iter()
+            .map(|arg| {
+                if let Some(v) = arg.downcast_ref::<i32>() {
+                    format!("{:?}", v)
+                } else {
+                    format!("{:?}", "<non-debug-arg>")
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+        println!(
+            "alter {}: {},{},{},[{}]",
+            _ctx.function_name, _ctx.module_path, _ctx.location.file, _ctx.location.line, args
+        );
+    }
+
+    fn around(&self, pjp: ProceedingJoinPoint) -> Result<Box<dyn Any>, AspectError> {
+        self.before(pjp.context());
+        pjp.proceed()
+    }
+}
+
+
+#[derive(Default)]
+pub struct Logger1;
+
+impl AsyncAspect for Logger1 {
     async fn before(&self, ctx: &AsyncJoinPoint) {
         let args = ctx
             .args
@@ -43,8 +95,34 @@ impl AsyncAspect for Logger {
             .join(", ");
 
         println!(
-            "{}: {},{},{},[{}]",
+            "before {}: {},{},{},[{}]",
             ctx.function_name, ctx.module_path, ctx.location.file, ctx.location.line, args
         );
     }
+
+    async fn after(&self, _ctx: &AsyncJoinPoint, _result: &(dyn Any + Send + Sync)) {
+        let args = _ctx
+            .args
+            .iter()
+            .map(|arg| {
+                if let Some(v) = arg.downcast_ref::<i32>() {
+                    format!("{:?}", v)
+                } else {
+                    format!("{:?}", "<non-debug-arg>")
+                }
+            })
+            .collect::<Vec<_>>()
+            .join(", ");
+
+        println!(
+            "after {}: {},{},{},[{}]",
+            _ctx.function_name, _ctx.module_path, _ctx.location.file, _ctx.location.line, args
+        );
+    }
+
+    async fn around(&self, pjp: AsyncProceedingJoinPoint<'_>) -> Result<Box<dyn Any + Send + Sync>, AspectError> {
+        self.before(pjp.context()).await;
+        Ok((pjp.proceed().await?))
+    }
+
 }
